fe9ace032883b04c6c24b4aad324a280b6301061
Savin <savin@angelmario.eu>
2023-04-16 23:13:03 +0300

    Renamed package name to org.eu.ciorogarlaunita.app

--- app/src/main/java/org/eu/ciorogarlaunita/app/Application.java
+++ app/src/main/java/org/eu/ciorogarlaunita/app/Application.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.eu.ciorogarlaunita.app.twa;
+package org.eu.ciorogarlaunita.app;
 
 
 
--- app/src/main/java/org/eu/ciorogarlaunita/app/DelegationService.java
+++ app/src/main/java/org/eu/ciorogarlaunita/app/DelegationService.java
@@ -1,4 +1,4 @@
-package org.eu.ciorogarlaunita.app.twa;
+package org.eu.ciorogarlaunita.app;
 
 
 
